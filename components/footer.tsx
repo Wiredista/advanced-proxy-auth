9fce9ed18508363bcc344ebc132d29bd471533dc
Álvaro Antônio @ KASPARYAN-PC <alvaro@kasparyan.tec.br>
2025-04-03 20:10:42 -0300

    Feat: Add footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,23 @@
 export default function Footer() {
   return (
     <footer className="w-full max-w-md mt-8 text-center text-sm text-gray-500">
-      <p className="font-medium">Advanced Proxy Auth</p>
-      <p>Open source under MIT License</p>
-      <p>Developed by Kasparyan Technologies</p>
+      <p className="font-medium">
+        <a href="https://github.com/Wiredista/advanced-proxy-auth" className="text-gray-700 hover:underline">
+          Advanced Proxy Auth
+        </a>
+      </p>
+      <p>
+        Open source under&nbsp;
+        <a href="https://mit-license.org/" className="text-gray-700 hover:underline">
+          MIT License
+        </a>
+      </p>
+      <p>
+        Developed by&nbsp;
+        <a href="https://kasparyan.tec.br/" className="text-gray-700 hover:underline">
+          Kasparyan Technologies
+        </a>
+      </p>
     </footer>
   )
 }
